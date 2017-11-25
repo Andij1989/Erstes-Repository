3d573bf6b00fb8a1dabfb3343359790748018310
Andij1989 <33758938+Andij1989@users.noreply.github.com>
2017-11-25 20:38:59 +0100

    Update Übung 2.php
    fertig

--- Übung 2.php
+++ Übung 2.php
@@ -1,5 +1,28 @@
-
 <?php
-
+$a = 1;		//Zahlenbereich von...
+$b = 100;	//Zahlenbereich bis...
+function zahlenbereich($a, $b)
+{
+    return "Zahlenbereich von $a bis $b:\n";
+}
+echo zahlenbereich($a, $b);   
+for($i=$a; $i < $b+1; $i++) 
+{
+if ($i % 3 == 0 AND $i % 5 == 0) 
+{
+echo "<br>$i = too bar ";
+}
+else if ($i % 3 == 0) 
+{
+echo "<br>$i = too ";
+}
+else if ($i % 5 == 0) 
+{
+echo "<br>$i = bar  ";
+}
+else 
+{
+echo "<br>$i = x";
+}
+}
 ?>
-
