ba068ee009e24338cd95904a98021da955bded2c
Andij1989 <andreas_jeske@gmx.de>
2017-11-25 18:43:03 +0100

    Hochladen

---
+++ Übung 1.php
@@ -0,0 +1,16 @@
+<?php
+
+for($i=1; $i < 1001; $i++) 
+if ($i % 3 == 0 AND $i % 5 == 0) {
+echo "<br>$i = too bar ";
+}
+else if ($i % 3 == 0) {
+echo "<br>$i = too ";
+}
+else if ($i % 5 == 0) {
+echo "<br>$i = bar  ";
+}
+else {
+echo "<br>$i = x";
+}
+?>
