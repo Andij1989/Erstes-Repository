2d55305165585e9bfdd2c33e01a2f611a116124f
Andij1989 <33758938+Andij1989@users.noreply.github.com>
2017-11-25 18:46:53 +0100

    Update Übung 1.php
    Richtige Klammersetzung

--- Übung 1.php
+++ Übung 1.php
@@ -1,16 +1,22 @@
 <?php
 
 for($i=1; $i < 1001; $i++) 
-if ($i % 3 == 0 AND $i % 5 == 0) {
+{
+if ($i % 3 == 0 AND $i % 5 == 0) 
+{
 echo "<br>$i = too bar ";
 }
-else if ($i % 3 == 0) {
+else if ($i % 3 == 0) 
+{
 echo "<br>$i = too ";
 }
-else if ($i % 5 == 0) {
+else if ($i % 5 == 0) 
+{
 echo "<br>$i = bar  ";
 }
-else {
+else 
+{
 echo "<br>$i = x";
+}
 }
 ?>
