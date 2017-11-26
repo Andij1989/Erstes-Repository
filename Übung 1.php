17e123b219004a3ca4c9ca03be5cb76d28cf8998
Andij1989 <33758938+Andij1989@users.noreply.github.com>
2017-11-27 00:02:03 +0100

    Update Übung 1.php

--- Übung 1.php
+++ Übung 1.php
@@ -2,21 +2,21 @@
 
 for($i=1; $i < 1001; $i++) 
 {
-if ($i % 3 == 0 AND $i % 5 == 0) 
-{
-echo "<br>$i = too bar ";
-}
-else if ($i % 3 == 0) 
-{
-echo "<br>$i = too ";
-}
-else if ($i % 5 == 0) 
-{
-echo "<br>$i = bar  ";
-}
-else 
-{
-echo "<br>$i = x";
-}
+  if ($i % 3 == 0 AND $i % 5 == 0) 
+  {
+    echo "<br>$i = too bar ";
+  }
+  else if ($i % 3 == 0) 
+  {
+    echo "<br>$i = too ";
+  }
+  else if ($i % 5 == 0) 
+  {
+    echo "<br>$i = bar  ";
+  }
+  else 
+  {
+    echo "<br>$i = x";
+  }
 }
 ?>
